dffa7371d80bfe4c1551f0a053057c852e87b045
naomi <89711375+nanamizu01@users.noreply.github.com>
2021-11-25 19:13:12 -0300

    retirando comentario

--- lingoappTcc/app/src/main/java/com/example/lingoappTcc/TurmaAtividadesFragment.java
+++ lingoappTcc/app/src/main/java/com/example/lingoappTcc/TurmaAtividadesFragment.java
@@ -13,7 +13,7 @@ public class TurmaAtividadesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        
         return inflater.inflate(R.layout.fragment_turma_atividades, container, false);
     }
 }
