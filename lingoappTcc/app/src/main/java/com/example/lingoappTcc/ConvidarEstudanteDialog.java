b2c0407fd0eeadb8b9d132b82304f656762e5224
naomi <89711375+nanamizu01@users.noreply.github.com>
2022-01-19 19:04:20 -0300

    inicio popup

---
+++ lingoappTcc/app/src/main/java/com/example/lingoappTcc/ConvidarEstudanteDialog.java
@@ -0,0 +1,14 @@
+package com.example.lingoappTcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConvidarEstudanteDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_convidar_estudante);
+    }
+}
